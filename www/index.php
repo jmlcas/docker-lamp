cc2962ffa8756b77ed8372edecdd443e104defe4
José Maria Labarta <59398281+jmlcas@users.noreply.github.com>
2021-10-12 21:36:39 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Welcome to LAMP Infrastructure</title>
+        <title>Bienvenido a LAMP</title>
         <meta charset="utf-8"> 
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
@@ -8,7 +8,7 @@
     <body>
         <div class="container-fluid">
             <?php
-                echo "<h1>¡Hola, OpenWebinars te da la bienvenida!</h1>";
+                echo "<h1>¡Bienvenido al ejemplo de Lamp!</h1>";
 
                 $conn = mysqli_connect('db', 'root', 'test');   
                 $database = mysqli_select_db($conn, 'dbname');  
@@ -17,7 +17,7 @@
                 $result = mysqli_query($conn, $query);
 
                 echo '<table class="table table-striped">';
-                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                echo '<thead><tr><th></th><th>id</th><th>nombre</th></tr></thead>';
                 while($value = $result->fetch_array(MYSQLI_ASSOC)){
                     echo '<tr>';
                     echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
